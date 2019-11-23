603666a226bda68e03fecf914ed64dccda019632
SlimeVidas <dahan.isaac@gmail.com>
2019-11-23 22:33:53 +0200

    getAllPlaylist function debut

--- js/playlist.js
+++ js/playlist.js
@@ -38,20 +38,66 @@ function addPlaylist(name, image, songs) {
     return song;
   }
 
+  //DISPLAY ALL THE PLAYLISTS
+
+  var getAllPlaylist = function(){
+    $.ajax({
+      url: 'http://localhost/playlist2019/api/playlist',
+
+      method: 'GET',
+      // data: {
+      //   'name': name,
+      //   'image': image,
+      //   'songs': songs
+      // }
+
+    }).done(function (response) {
+      console.log(response.data);
+      for(var i = 0;i< response.data.length; i++){
+        console.log(response.data[0].image)
+        console.log(response.data[1].image)
+
+
+        $('.mainPlaylist').append( `
+        <div class="col s12 m6 l4 xl3 playlist">
+          <img src="${response.data[i].image}"  alt="" class="center">
+          <div class="arcText">${response.data[i].name}"</div>
+          <i class="material-icons playBtn">
+              play_circle_outline
+              </i>
+              <div class="actions">
+                  <i class="material-icons cancel">
+                      cancel
+                      </i>
+        
+                      <i class="material-icons edit">
+                          edit
+                          </i>"
+                          </div>
+                          `)
+
+      }
+
+    }).fail(function (textStatus) {
+      console.log(textStatus);
+    });
+  }
+
   
 
 return{
     _addPlaylist:addPlaylist,
-    _insertSongToPlaylist:insertSongToPlaylist
+    _insertSongToPlaylist:insertSongToPlaylist,
+    _getAllPlaylist:getAllPlaylist
 }
 })();
 
 
 var processPlaylist = (function(){
+
+  playlist._getAllPlaylist();
   $('#modal-add-songs .finishAndSave').click(function(){
     // playlist._addPlaylist();
-
-    
 
     $name = $("input[name='playlist_name']").val();
     $image = $("input[name='playlist_url']").val();
@@ -64,8 +110,10 @@ var processPlaylist = (function(){
     playlist._addPlaylist($name, $image, songs);
     // $('#addPlaylistModal').modal('toggle');
     // $('.playlist').html('');
-    // getAllPlaylists();
-});
+    playlist._getAllPlaylist()
+  });
+
+
 
 })();
 
