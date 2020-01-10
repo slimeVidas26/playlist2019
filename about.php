6b2d4b9c94bee8ff6acd3f361315a8885303d7c1
SlimeVidas <dahan.isaac@gmail.com>
2020-01-10 15:50:15 +0200

    corrections

--- about.php
+++ about.php
@@ -1,4 +1,13 @@
-<div class="navbar-fixed ">
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php require('components/head.html') ?>
+</head>
+
+<body>
+  <nav role="navigation">
+  <div class="navbar-fixed ">
     <nav>
     <div class="nav-wrapper">
       <ul class="left hide-on-med-and-down">
@@ -8,12 +17,7 @@
                  Home
               </a>
             </li>
-        <li>
-          <!-- Modal Trigger -->
-            <a class=" modal-trigger" href="#modalAdd">
-            <i class="material-icons left addPlaylist">add_circle </i>Add New Playlist
-          </a>
-        </li>
+       
 
         <li>
             <!-- Modal Trigger -->
@@ -43,3 +47,44 @@
     </div>
   </nav>
 </div>
+  </nav>
+
+  
+  <div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+      <br><br>
+     
+
+
+
+     
+      <br><br>
+    </div>
+  </div>
+  <div class="pl-container">
+  <img src="Docs/resume.jpg" alt="">
+  </div>
+  <br><br>
+  </div>
+  
+  <footer class="page-footer orange">
+    <?php require('components/footer.html') ?>
+
+  </footer>
+
+  <!--  Scripts-->
+
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/jquery-text-marquee@latest/dist/jquery.text-marquee.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+  
+
+
+
+
+  
+
+</body>
+
+</html>
